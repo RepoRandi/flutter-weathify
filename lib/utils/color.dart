cac7baf43db3bf60922bd5ca174e21a1c3fd24b9
Randi <randimaulanaakbar@gmail.com>
2024-03-09 16:09:38 +0700

    feat: add colors

---
+++ lib/utils/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color backgroundColor = Colors.black;
+const Color deepPurpleColor = Colors.deepPurple;
+const Color purpleColor = Color(0XFF673AB7);
+const Color orangeColor = Color(0XFFFFAB40);
