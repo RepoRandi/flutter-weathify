c13c756071fd52b95fb42e791905bb9fc63cf1cb
Randi <randimaulanaakbar@gmail.com>
2024-03-09 16:51:07 +0700

    feat: add widget date time

---
+++ lib/widgets/date_time.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DateTimeWidget extends StatefulWidget {
+  const DateTimeWidget({super.key});
+
+  @override
+  State<DateTimeWidget> createState() => _DateTimeWidgetState();
+}
+
+class _DateTimeWidgetState extends State<DateTimeWidget> {
+  String _currentDateTime = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _updateDateTime();
+  }
+
+  void _updateDateTime() {
+    setState(() {
+      _currentDateTime = _getCurrentDateTime();
+    });
+
+    Future.delayed(const Duration(seconds: 1), _updateDateTime);
+  }
+
+  String _getCurrentDateTime() {
+    DateTime now = DateTime.now();
+    String formattedDate = DateFormat('EEEE, dd MMMM yyyy').format(now);
+    String formattedTime = DateFormat('HH:mm:ss').format(now);
+    return '$formattedDate | $formattedTime';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _currentDateTime,
+      style: const TextStyle(
+          color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+    );
+  }
+}
